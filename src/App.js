78cee047fb84d41b4458d79fb35b402c30b7c669
tschernysh <tschernysh@gmail.com>
2021-05-26 00:35:14 +0300

    fixed header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import s from './App.module.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import {Route} from 'react-router-dom'
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <div className={s.container}>
+        <Route path='/' render={() => <Main />} />
+      </div>
       <Footer />
     </div>
   );
