96b753044c65c2d7d0a55f4edc729719a68b23f5
tschernysh <tschernysh@gmail.com>
2021-05-31 21:43:28 +0300

    part

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -3,6 +3,8 @@ import {useState} from "react";
 
 const ShoppingCart = () => {
     const [address, setAddress] = useState({city: 'Киев', division: '1 отделение'})
+    const [delivery, setDelivery] = useState('self')
+    const [payment, setPayment] = useState('imposed')
 
     return(
         <div className={s.cart}>
@@ -12,36 +14,89 @@ const ShoppingCart = () => {
                 </div>
                 <div className={s.cart__block}>
                     <div className={s.cart__info}>
+
                         <div className={s.cart__delivery}>
                             <h5>1. Способ доставки</h5>
                             <div className={s.cart__delivery_radio}>
-                                <div>
-                                    <input name='type' type="radio"/>
+                                <label onClick={() => setDelivery('self')}>
+                                    <input checked={delivery === 'self'} name='type' type="radio"/>
+                                    <span className={`${s.radio__styled} ${delivery === 'self' ? s.radio__active : ''}`}></span>
                                     <span>Самовывоз</span>
-                                </div>
-                                <div>
-                                    <input name='type' type="radio"/>
+                                </label>
+                                <label onClick={() => setDelivery('np')}>
+                                    <input checked={delivery === 'np'} name='type' type="radio"/>
+                                    <span className={`${s.radio__styled} ${delivery === 'np' ? s.radio__active : ''}`}></span>
                                     <span>Нова пошта</span>
-                                </div>
+                                </label>
                             </div>
                             <div className={s.cart__delivery_address}>
                                 <h5>Ваш адрес</h5>
                                 <p>
                                     <span>Город:</span>
-                                    <select name={'city'} value={address.city}>
+                                    <select className={s.styled__select} name={'city'} value={address.city}>
                                         <option value={"Винница"}>Винница</option>
                                         <option value={"Киев"}>Киев</option>
                                     </select>
                                 </p>
                                 <p>
                                     <span>Отделение №:</span>
-                                    <select name={'division'} value={address.division}>
+                                    <select className={s.styled__select} name={'division'} value={address.division}>
                                         <option value="1">1 Отделение</option>
                                         <option value="2">2 Отделение</option>
                                     </select>
                                 </p>
                             </div>
                         </div>
+
+                        <div className={s.cart__name}>
+                            <h5>2. Данные получателя</h5>
+                            <div className={s.cart__name_forms}>
+                                <div className={s.name__form}>
+                                    <p>Имя</p>
+                                    <input placeholder={'Иван'} type="text"/>
+                                </div>
+                                <div className={s.name__form}>
+                                    <p>Номер</p>
+                                    <input placeholder={'+380 66666666'} type="tel" />
+                                </div>
+                                <div className={s.name__form}>
+                                    <p>E-mail</p>
+                                    <input placeholder={'@gmail.com'} type="email"/>
+                                </div>
+                                <div className={s.name__form}>
+                                    <p>ФИО получателя</p>
+                                    <input placeholder={'Иванов Иван Иванович'} type="text"/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="cart__payment">
+                            <h5>Способ оплаты</h5>
+                            <div className="cart__payment_inputs">
+                                <div className="cart__payment_input">
+                                    <label onClick={() => setPayment('imposed')}>
+                                        <input checked={payment === 'imposed'} name='payment' type="radio"/>
+                                        <span className={`${s.radio__styled} ${payment === 'imposed' ? s.radio__active : ''}`}></span>
+                                        <span>Наложенный платеж</span>
+                                    </label>
+                                </div>
+                                <div className="cart__payment_input">
+                                    <label onClick={() => setPayment('prepayment')}>
+                                        <input checked={payment === 'prepayment'} name='payment' type="radio"/>
+                                        <span className={`${s.radio__styled} ${payment === 'prepayment' ? s.radio__active : ''}`}></span>
+                                        <span>Предоплата на карту</span>
+                                    </label>
+                                </div>
+                                <div className="cart__payment_input">
+                                    <label onClick={() => setPayment('cash')}>
+                                        <input checked={payment === 'cash'} name='payment' type="radio"/>
+                                        <span className={`${s.radio__styled} ${payment === 'cash' ? s.radio__active : ''}`}></span>
+                                        <span>Наличными или картой в автосалоне</span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                     <div className={s.cart__product}></div>
                 </div>
