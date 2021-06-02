a90698483a0beacf448699542e24f2913d725366
tschernysh <tschernysh@gmail.com>
2021-06-03 01:05:08 +0300

    fixes

--- src/components/Main/Bestsellers/Bestsellers.jsx
+++ src/components/Main/Bestsellers/Bestsellers.jsx
@@ -1,18 +1,40 @@
 import s from './Bestsellers.module.css'
 import BestsellersItem from "./BestsellersItem/BestsellersItem";
+import {useState} from "react";
+import {ReactComponent as ArrowLeft} from "../../../common/icons/arrow-left.svg";
+import {ReactComponent as ArrowRight} from "../../../common/icons/arrow-right.svg";
 
 const Bestsellers = () => {
+    const [page, setPage] = useState(1)
+    const [full, setFull] = useState(false)
+
     return (
         <div className={s.mainPage__bestsellers}>
-            <h1 className={s.mainPage__bestsellers_h1}>Хиты продаж</h1>
+            <div className={s.mainPage__bestsellers_title}>
+                <h1 className={s.mainPage__bestsellers_h1}>Хиты продаж</h1>
+                <div className={s.mainPage__bestsellers_dots}>
+                    <span className={page === 1 ? s.active__dot : ''}></span>
+                    <span className={page === 2 ? s.active__dot : ''}></span>
+                    <span className={page === 3 ? s.active__dot : ''}></span>
+                </div>
+            </div>
             <div className={s.mainPage__bestsellers_wrap}>
+                <button onClick={() => {setPage(page-1 !== 0 ? page-1 : 3)}} className={s.left}><ArrowLeft/></button>
                 <BestsellersItem/>
                 <BestsellersItem/>
                 <BestsellersItem/>
                 <BestsellersItem/>
+                {full && <>
+                <BestsellersItem/>
+                <BestsellersItem/>
+                <BestsellersItem/>
+                <BestsellersItem/>
+                </>
+                }
+                <button onClick={() => {setPage(page+1 > 3 ? 1 : page+1)}} className={s.right}><ArrowRight/></button>
             </div>
             <div className={s.mainPage__bestsellers_btn}>
-                <a href="">Посмотреть всё</a>
+                <a onClick={() => setFull(!full)}>{full ? "Скрыть всё" : "Посмотреть всё"}</a>
             </div>
         </div>
     )
--- src/components/Main/Clients/Clients.jsx
+++ src/components/Main/Clients/Clients.jsx
@@ -1,21 +1,42 @@
 import s from './Clients.module.css'
 import ClientsItem from "./ClientsItem/ClientsItem";
+import {useState} from "react";
+import {ReactComponent as ArrowLeft} from "../../../common/icons/arrow-left.svg";
+import {ReactComponent as ArrowRight} from "../../../common/icons/arrow-right.svg";
 
 const Clients = () => {
+    const [page, setPage] = useState(1)
+    const [full, setFull] = useState(false)
+
     return (
         <div className={s.mainPage__ourClientsrecommend}>
             <div className={s.container}>
-                <h1 className={s.mainPage__OurClientsrecommend_h1}>Наши клиенты рекомендуют</h1>
+                <div className={s.mainPage__clients_title}>
+                    <h1 className={s.mainPage__OurClientsrecommend_h1}>Наши клиенты рекомендуют</h1>
+                    <div className={s.mainPage__bestsellers_dots}>
+                        <span className={page === 1 ? s.active__dot : ''}></span>
+                        <span className={page === 2 ? s.active__dot : ''}></span>
+                        <span className={page === 3 ? s.active__dot : ''}></span>
+                    </div>
+                </div>
                 <div className={s.mainPage__OurClientsrecommend_wrap}>
-
+                    <button onClick={() => {setPage(page-1 !== 0 ? page-1 : 3)}} className={s.left}><ArrowLeft/></button>
                     <ClientsItem/>
                     <ClientsItem/>
                     <ClientsItem/>
                     <ClientsItem/>
-
+                    {
+                        full && <>
+                            <ClientsItem/>
+                            <ClientsItem/>
+                            <ClientsItem/>
+                            <ClientsItem/>
+                        </>
+                    }
+                    <button onClick={() => {setPage(page+1 > 3 ? 1 : page+1)}} className={s.right}><ArrowRight/></button>
                 </div>
                 <div className={s.mainPage__bestsellers_btn}>
-                    <a href="">Посмотреть всё</a>
+                    <a onClick={() => setFull(!full)}>{full ? "Скрыть всё" : "Посмотреть всё"}</a>
                 </div>
 
             </div>
